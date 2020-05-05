a92c1b9c5cd830093667611d062960f605ee6010
Fabian LLanos <fa.llanos@udla.edu.co>
2020-05-05 00:00:07 -0500

    Interfaces

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
+using CoreEscuela.Util;
 
 namespace CoreEscuela.Entidades
 {
-    public class Escuela : ObjetoEscuelaBase
+    public class Escuela : ObjetoEscuelaBase, ILugar
     {
 
         public int AnioDeCreacion { get; set; }
         public string Pais { get; set; }
         public string Ciudad { get; set; }
+        public string Direccion { get; set; }
 
         public TiposEscuela TipoEscuela { get; set; }
         public List<Curso> Cursos { get; set; }
@@ -24,6 +26,17 @@ namespace CoreEscuela.Entidades
         public override string ToString()
         {
             return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine}Pais: {Pais}, Ciudad: {Ciudad}, Año de Creacion: {AnioDeCreacion}";
+        }
+        public void LimpiarLugar()
+        {
+            Printer.DrawLine();
+            Console.WriteLine("Limpiando Escuela");
+            foreach (var curso in Cursos)
+            {
+                curso.LimpiarLugar();
+
+            }
+            Console.WriteLine($"Escuela {Nombre} Esta limpia...");
         }
     }
 }
